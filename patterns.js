739ac1819b0eb2cbeebd3a25d9667e1925decc30
sceretgirl328-art <sceretgirl328@gmail.com>
2026-01-03 16:39:59 +0530

    Delete patterns.js

--- patterns.js
+++
@@ -1,9 +0,0 @@
-// patterns.js
-export function normalizeSymptom(symptom){
-    return symptom.toLowerCase().replace(/\s+/g,'');
-}
-
-export function matchSymptoms(userSymptoms, diseaseSymptoms){
-    return userSymptoms.map(normalizeSymptom)
-                       .filter(sym => diseaseSymptoms.hasOwnProperty(sym));
-}
