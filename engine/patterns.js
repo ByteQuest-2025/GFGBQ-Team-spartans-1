83a2b4ab6d2d4766a6d919d113184ba0675c6e68
sceretgirl328-art <sceretgirl328@gmail.com>
2026-01-03 19:09:17 +0530

    Create patterns.js
    Completed

---
+++ engine/patterns.js
@@ -0,0 +1,19 @@
+export function normalizeInputs(data) {
+    return {
+        family_history: data.family_history,
+        
+        bilirubin_trend: Math.min((data.bilirubin_trend || 0) / 2, 1),
+        
+        cholesterol: data.cholesterol > 250 ? 1 : (data.cholesterol - 150) / 100,
+        
+        ggt: data.ggt > 100 ? 1 : (data.ggt / 100),
+        
+        activity: data.activity < 5 ? (5 - data.activity) / 5 : 0,
+        
+        sleep: data.sleep < 8 ? (8 - data.sleep) / 8 : 0,
+        
+        nutrition: data.nutrition < 70 ? (70 - data.nutrition) / 70 : 0,
+        
+        stress: data.stress > 50 ? (data.stress - 50) / 50 : 0
+    };
+}
